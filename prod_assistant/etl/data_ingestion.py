82f02b814f0277a548142a8600ad4e3062d69cd5
jeebesh-chandra-podder <jeebeshchandrapodder0@gmail.com>
2025-09-16 22:27:52 +0530

    Remove unnecessary blank line in DataIngestion class

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -40,7 +40,6 @@ class DataIngestion:
         self.db_application_token = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
         self.db_keyspace = os.getenv("ASTRA_DB_KEYSPACE")
 
-       
 
     def _get_csv_path(self):
         """
